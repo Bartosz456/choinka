aeb4965c67aeb6d971de74dc0efd4104e0d9c8d0
Bartosz456 <bartoszsz14@gmail.com>
2022-10-27 21:57:33 +0200

    cpp

---
+++ funkcje.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+
+using namespace std;
+void gwiazdka(){
+	cout << "*";
+}
+void spacja(){
+	cout << " ";
+}
+int main()
+{
+	int w, j, z;
+	cout << "Podaj liczbe wierszy: ";
+	cin >> w;
+	z = w;
+	for (int i = 1; i <= w; i++)
+	{
+		for (int z = w - 1; z >= i; z--)
+		{
+
+			spacja();
+		}
+
+		for (j = 1; i * 2 - 1 >= j; j++)
+		{
+			gwiazdka();
+		}
+
+
+		cout << endl;
+	}
+
+
+	return 0;
+}
